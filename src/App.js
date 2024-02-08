baae49249ca454fa1c233953f6157900752e6ff9
umairahfodkar <umairah.fodkar@sakec.ac.in>
2024-02-08 12:50:43 +0530

    added h1

--- src/App.js
+++ src/App.js
@@ -2,15 +2,13 @@
 import './App.css';
 import Footer from './components/Footer';
 import About from './components/About';
-<<<<<<< HEAD
 import Services from './components/Services';
-=======
 import Contact from './components/Contact';
->>>>>>> 5fc2a85b1133c3381eb1c8aa1ef994f24f20fce9
 function App() {
   return (
     <div className="App">
       <Services/>
+      <h1>Creating the kitchen</h1>
       <About />
 <Contact />
       <Footer />
