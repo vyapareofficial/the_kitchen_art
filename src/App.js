5fc2a85b1133c3381eb1c8aa1ef994f24f20fce9
vyapareofficial <vyapareofficial@gmail.com>
2024-02-08 12:42:13 +0530

    contact added

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@
 import './App.css';
 import Footer from './components/Footer';
 import About from './components/About';
+import Contact from './components/Contact';
 function App() {
   return (
     <div className="App">
       <h1>sachin</h1>
       <About />
+<Contact />
       <Footer />
     </div>
   );
