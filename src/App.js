f5cecfbb199b7305c5ba797843d0d2eb39ba52eb
umairahfodkar <umairah.fodkar@sakec.ac.in>
2024-02-08 12:42:37 +0530

    added component

--- src/App.js
+++ src/App.js
@@ -2,10 +2,11 @@
 import './App.css';
 import Footer from './components/Footer';
 import About from './components/About';
+import Services from './components/Services';
 function App() {
   return (
     <div className="App">
-      <h1>sachin</h1>
+      <Services/>
       <About />
       <Footer />
     </div>
