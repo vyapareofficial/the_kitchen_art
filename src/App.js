6294efb9ba43337ece00758ef1b84036e226c8a6
vyapareofficial <vyapareofficial@gmail.com>
2024-02-13 16:01:49 +0530

    testimonial added

--- src/App.js
+++ src/App.js
@@ -5,12 +5,14 @@ import About from './components/About';
 import Services from './components/Services';
 import Contact from './components/Contact';
 import Steps from './components/Steps';
+import Testimonials from './components/Testimonials';
 function App() {
   return (
     <div className="App">
       <Services/>
       <About />
       <Steps />
+      <Testimonials />
       <Contact />
       <Footer />
     </div>
